8ff6605043fa9e456594fc12eccabd54dd419898
kuhanimkuu <davidmurunga@gmail.com>
2025-05-31 13:48:25 +0300

    fifth commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './NavBar.css'
 import menu_icon from '../../assets/menu.png'
 import logo from '../../assets/logo.png'
@@ -9,6 +9,18 @@ import notification_icon from '../../assets/notification.png'
 import profile_icon from '../../assets/jack.png'
 import { Link } from 'react-router-dom'
 function NavBar({setSidebar}) {
+  const fetchData = async () =>{
+        const videoList_URL =`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=Ke&videoCategoryId=${category}&key=${API_KEY}`
+        await fetch(videoList_URL)
+        .then(response=>response.json()).then(data=>setData(data.items))
+        .catch((error)=>console.log(error));
+     
+      }
+      useEffect(()=>{
+        fetchData()
+      },[])
+
+
   return (
     <nav className='flex-div'>
       <div className="nav-left flex-div">
@@ -21,7 +33,7 @@ function NavBar({setSidebar}) {
       <div className="nav-middle flex-div">
         <div className="search-box flex-div">
           <input type='text' placeholder='Search'/>
-        <img src={search_icon} alt=''/>
+        <img src={search_icon} alt='' onClick={()=>{}}/>
         </div>
         
       </div>
--- src/components/playVideo/PlayVideo.jsx
+++ src/components/playVideo/PlayVideo.jsx
@@ -44,7 +44,7 @@ fetchOtherData()
   return (
     <div className='play-video'>
      
-      <iframe  src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe  src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;"  referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       <h3>{apiData?apiData.snippet.title:"Title here"}</h3>
       <div className="play-video-info">
         <p>{apiData?valueConverter(apiData.statistics.viewCount):"16K"} &bull; {apiData?moment(apiData.snippet.publishedAt).fromNow():""}</p>
